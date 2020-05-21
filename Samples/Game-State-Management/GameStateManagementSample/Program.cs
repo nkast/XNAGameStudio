2bed65dd404d23660a8cf5f0c831f6f2d2bf26d8
Nick Kastellanos <nkastellanos@gmail.com>
2020-05-21 18:12:42 +0300

    Extract GameStateManagement Sample

---
+++ Samples/Game-State-Management/GameStateManagementSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameStateManagementSample
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (GameStateManagementSampleGame game = new GameStateManagementSampleGame())
+                game.Run();
+        }
+    }
+#endif
+}
